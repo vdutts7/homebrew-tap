5a6345f42a0c697bda032e4fc70a101f8a0d7b49
vdutts7 <vdutts7@users.noreply.github.com>
2026-02-01 12:05:04 -0800

    Add shelllock formula

---
+++ Formula/shelllock.rb
@@ -0,0 +1,18 @@
+class Shelllock < Formula
+  desc "Protect any shell command behind Touch ID"
+  homepage "https://github.com/vdutts7/shelllock-macos"
+  url "https://github.com/vdutts7/shelllock-macos.git", tag: "v1.0.0"
+  license "MIT"
+  head "https://github.com/vdutts7/shelllock-macos.git", branch: "main"
+
+  depends_on :macos
+
+  def install
+    system "make", "build-fast"
+    bin.install "bin/shelllock"
+  end
+
+  test do
+    assert_match "shelllock", shell_output("#{bin}/shelllock --version")
+  end
+end
